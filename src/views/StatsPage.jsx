572b688771d5cbcadcce6323d57c67bd54bda1ec
Vasu Gazula <vasugazula@gmail.com>
2019-03-11 23:14:03 -0700

    updated with popup

--- src/views/StatsPage.jsx
+++ src/views/StatsPage.jsx
@@ -2,9 +2,21 @@ import React from "react";
 // react plugin used to create charts
 // reactstrap components
 import {
-  Row, Col,
-  UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem
+  Row,
+  Col,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ButtonGroup
 } from "reactstrap";
+
+import NumericInput from 'react-numeric-input';
 
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.jsx";
@@ -15,8 +27,13 @@ class StatsPage extends React.Component {
     super(props);
     this.state = {
       weapon1: {},
-      weapon2: {}
+      weapon2: {},
+      modalDemo: false,
+      rSelected: 0
     };
+
+    this.toggleModalDemo = this.toggleModalDemo.bind(this);
+    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
   }
   componentDidMount() {
     document.body.classList.toggle("landing-page");
@@ -41,13 +58,31 @@ class StatsPage extends React.Component {
     this.setState({weapon2 : selectedWeapon[0]});
   }
 
+  toggleModalDemo(){
+    this.setState({
+      modalDemo: !this.state.modalDemo
+    });
+  }
+
+  onRadioBtnClick(number) {
+    this.setState({rSelected: number});
+  }
+
   render() {
     let weaponDropdownItems = this.props.weapons.map((weap,index) => {
       return(<DropdownItem key={index} ref={this.dropDownSelection} onClick={this.weaponSelector}>{weap.name}</DropdownItem>)
     });
+    let inscriptionDropdownItems = this.props.inscriptions.map((insc,index) => {
+      return(<DropdownItem key={index} ref={this.dropDownSelection} >{insc}</DropdownItem>)
+    });
     let weaponDropdownItems2 = this.props.weapons.map((weap,index) => {
       return(<DropdownItem key={index} ref={this.dropDownSelection} onClick={this.weaponSelector2}>{weap.name}</DropdownItem>)
     })
+    let styles = {
+      width: '50px',
+      height: '50px',
+      display: 'inline-block',
+    };
     return (
       <>
         <ExamplesNavbar />
@@ -97,6 +132,72 @@ class StatsPage extends React.Component {
                       {weaponDropdownItems}
                     </DropdownMenu>
                   </UncontrolledDropdown>
+                  <br/>
+                  <Button className="btn-round btn-icon" color="default" onClick={this.toggleModalDemo}>
+                    <i className="tim-icons icon-notes" />
+                  </Button>
+                  <Modal isOpen={this.state.modalDemo} toggle={this.toggleModalDemo} modalClassName="modal-black">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Inscriptions
+                      </h5>
+                      <button
+                          type="button"
+                          className="close"
+                          data-dismiss="modal"
+                          aria-hidden="true"
+                          onClick={this.toggleModalDemo}
+                      >
+                        <i className="tim-icons icon-simple-remove" />
+                      </button>
+                    </div>
+                    <ModalBody>
+                      <ButtonGroup>
+                        <Button size="sm" onClick={() => this.onRadioBtnClick(1)}
+                                active={this.state.rSelected === 1}><img style={styles}
+                                                                         src={require("assets/img/gear-icon.png")}
+                                                                         alt="my image"/></Button>
+                        <Button size="sm" onClick={() => this.onRadioBtnClick(1)}
+                                active={this.state.rSelected === 1}><img style={styles}
+                                                                         src={require("assets/img/suit-icon.png")}
+                                                                         alt="my image"/></Button>
+                      </ButtonGroup>
+                      <br/>
+                      <br/>
+                      <NumericInput min={0} max={300} value={0}/>
+                      <br/>
+                      <br/>
+                      <UncontrolledDropdown group>
+                        <DropdownToggle caret data-toggle="dropdown">
+                          Select Inscription ...
+                        </DropdownToggle>
+                        <DropdownMenu
+                            modifiers={{
+                              setMaxHeight: {
+                                enabled: true,
+                                order: 890,
+                                fn: (data) => {
+                                  return {
+                                    ...data,
+                                    styles: {
+                                      ...data.styles,
+                                      overflow: 'auto',
+                                      maxHeight: 300,
+                                    },
+                                  };
+                                },
+                              },
+                            }} >
+                          {inscriptionDropdownItems}
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button color="primary">
+                        Save
+                      </Button>
+                    </ModalFooter>
+                  </Modal>
                 </Col>
                 <Col lg="6" md="6">
                   <h1 className="text-white">
@@ -111,41 +212,14 @@ class StatsPage extends React.Component {
                 </Col>
               </Row>
               <br/><br/><br/>
-              <Row className="row-grid justify-content-between align-items-start text-left">
-                <Col lg="6" md="6">
-                  <h1 className="text-white">
-                    Weapon Slot 2
-                  </h1>
-                  <UncontrolledDropdown group>
-                    <DropdownToggle caret color="danger" data-toggle="dropdown">
-                      Select your secondary gun
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownMenu>
-                        {weaponDropdownItems2}
-                      </DropdownMenu>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                  <br/>
-                </Col>
-                <Col lg="6" md="6">
-                  <h1 className="text-white">
-                    {this.state.weapon2.name}
-                  </h1>
-                  <h4 className="text-white">
-                    {this.state.weapon2.description}
-                  </h4>
-                  <h4 className="text-warning">
-                    {this.state.weapon2.special}
-                  </h4>
-                </Col>
-              </Row>
             </div>
           </div>
         </div>
       </>
     );
   }
+
+
 }
 
 export default StatsPage;
